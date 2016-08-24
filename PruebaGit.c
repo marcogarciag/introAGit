cec6c84b208905dc39e54f48b3d643db6d80e39f
marcotulio105@gmail.com <MarcoGarcia@Marcos-MacBook-Pro.local>
2016-08-24 11:40:25 -0500

    en este commit agregue hola git

---
+++ PruebaGit.c
@@ -0,0 +1,15 @@
+//
+//  PruebaGit.c
+//  
+//
+//  Created by Marco Tulio García González on 24/08/16.
+//
+//
+
+#include "PruebaGit.h"
+
+int main ()
+{
+    printf ("Hola Git, bienvenido\n");
+    return 0;
+}
---
+++ PruebaGit.h
@@ -0,0 +1,14 @@
+//
+//  PruebaGit.h
+//  
+//
+//  Created by Marco Tulio García González on 24/08/16.
+//
+//
+
+#ifndef PruebaGit_h
+#define PruebaGit_h
+
+#include <stdio.h>
+
+#endif /* PruebaGit_h */
