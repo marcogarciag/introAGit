e114240c75d9ba0c0ae563ed85c51eac2483e28c
marcotulio105@gmail.com <MarcoGarcia@Marcos-MacBook-Pro.local>
2016-08-24 11:59:43 -0500

    segunda subida

---
+++ SegundoProg.c
@@ -0,0 +1,24 @@
+//
+//  SegundoProg.c
+//  
+//
+//  Created by Marco Tulio García González on 19/08/16.
+//
+//
+
+#include <stdio.h>
+int main ()
+
+{
+    int valor1=0;
+    int valor2=0;
+    int suma;
+    printf ("Introduzca el primer número:\n");
+    scanf ("%d", &valor1);
+    printf("Introduzca el segundo número:\n");
+    scanf ("%d", &valor2);
+    suma=(valor1+valor2);
+    printf("la suma es %d \n", suma);
+    return 0;
+}
+
