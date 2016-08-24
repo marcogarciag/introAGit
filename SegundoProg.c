271fac80939e6f9d18317aa1550236861edba481
marcotulio105@gmail.com <MarcoGarcia@Marcos-MacBook-Pro.local>
2016-08-24 12:24:31 -0500

    cambio para comparación

--- SegundoProg.c
+++ SegundoProg.c
@@ -10,12 +10,16 @@
 int main ()
 
 {
+    //hola
     int valor1=0;
     int valor2=0;
+    //adiós
     int suma;
     printf ("Introduzca el primer número:\n");
     scanf ("%d", &valor1);
     printf("Introduzca el segundo número:\n");
+    //equis
+    sdfsf
     scanf ("%d", &valor2);
     suma=(valor1+valor2);
     printf("la suma es %d \n", suma);
