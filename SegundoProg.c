1b0c2226e1ff35807067e2e49bcb9c81ec702cda
marcogarciag <marcotulio105@gmail.com>
2016-08-24 12:22:43 -0500

    Update SegundoProg.c

--- SegundoProg.c
+++ SegundoProg.c
@@ -10,15 +10,21 @@
 int main ()
 
 {
+    //hola3
     int valor1=0;
+    //hola4
+    iiihg
+    oiiuh
     int valor2=0;
     int suma;
     printf ("Introduzca el primer número:\n");
     scanf ("%d", &valor1);
     printf("Introduzca el segundo número:\n");
+    //hola2
     scanf ("%d", &valor2);
     suma=(valor1+valor2);
     printf("la suma es %d \n", suma);
     return 0;
+    //hola
 }
 
